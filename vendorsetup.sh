59f090b084cb77c1ae4a0f2b709e84fc672a866c
AT WORLD <atharvsutar3102003@gmail.com>
2022-07-24 11:43:54 +0530

    sdm660-common: Setup Prebuilts_tools-extra

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,4 @@
+rm -rf prebuilts/tools-extras
+
+git clone https://github.com/ATBlastDon/android_prebuilts_tools-extras.git -b twelve prebuilts/tools-extras
 git clone https://github.com/ATBlastDon/packages_apps_GoogleCameraGoEdition.git -b eleven packages/apps/GoogleCameraGoEdition
